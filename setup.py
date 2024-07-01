ce58b3cd15557784a4d931a60b7449ff50188650
yaronetokayer <109242322+yaronetokayer@users.noreply.github.com>
2024-07-01 16:39:48 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pynemo',
+    version='0.0.1',
+    author='Yarone Tokayer',
+    author_email='yarone.tokayer@yale.edu',
+    description='A collection of functions for issuing commands for N body simulations using NEMO programs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/yaronetokayer/pynemo',
+    project_urls = {
+        "Bug Tracker": "https://github.com/yaronetokayer/pynemo/issues"
+    },
+    license='GNU GPLv3',
+    packages=[
+        'pynemo'
+    ],
+    install_requires=[
+    ],
+)
